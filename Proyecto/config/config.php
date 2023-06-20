813efebc369aacc16e11ee0875071e9fa8621519
Kikee11 <99348360+Kikee11@users.noreply.github.com>
2023-06-20 15:56:50 +0200

    Subida archivos

---
+++ Proyecto/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Database connection values */
+/* define("DB_HOST", "localhost");
+define("DB", "grancapitan");
+define("DB_USER", "root");
+define("DB_PASS", "test"); */
+
+/* Default options */
+define("DEFAULT_CONTROLLER", "tarjeta");
+define("DEFAULT_ACTION", "form");
